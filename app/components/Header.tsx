b59cb914f736aadbabdeab490fd88853a7e9c702
ruthless 59 <you@example.com>
2025-03-23 23:39:46 -0500

    updated header.tsx img config

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <header className="w-[100%] bg-sky-100 rounded-lg px-4 shadow-lg border-1 border-sky-200 mb-4 fixed">
         <nav className='flex justify-end items-center gap-6 p-4 bg-gradient-to-b from-white-100 to-white-700'>
-            <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/logo-drmTRP513F1ckyU3cudzZ1GR7nNJFt.png" alt='Pyramyd' className='h-[35px] w-[155px] mr-auto'/>
+            <Image src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/logo-drmTRP513F1ckyU3cudzZ1GR7nNJFt.png" alt='Pyramyd' width={155} height={35} className='h-[35px] w-[155px] mr-auto'/>
             <a href="https://www.pyramyd.ai/" className='hover:text-blue-500'>Contact Us</a>
             <button className='hover:text-blue-500 cursor-pointer'>Log-In</button>
             <button className='hover:text-blue-500 cursor-pointer'>Sign-In</button>
